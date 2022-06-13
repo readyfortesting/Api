a72ffd60e975119c5ebc50f52b4cbc4344fd7361
tallboy6077 <tallboy6077@gmail.com>
2022-06-13 18:09:05 +0300

    GMIBank03

---
+++ src/test/java/get_http_request/Day15/GMIBank03.java
@@ -0,0 +1,16 @@
+package get_http_request.Day15;
+
+public class GMIBank03 {
+        /*
+    http://www.gmibank.com/api/tp-customers end point'ine
+    request gönderin
+    First Name,
+    Last Name,
+    email,
+    mobilePhone,
+    Adres
+    city
+    Bilgilerini text dosyasına yazdırın
+     */
+
+}
